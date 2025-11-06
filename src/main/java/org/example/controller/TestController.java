a659cdba81bdeebb9c0fbf43960ab754185f3e2f
xz <xz-jasper@qq.com>
2025-11-06 21:50:43 +0800

    modify item  as springboot item

---
+++ src/main/java/org/example/controller/TestController.java
@@ -0,0 +1,17 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+
+    @GetMapping("/test")
+    public String testEndpoint() {
+        return "Test successful!";
+    }
+
+}
